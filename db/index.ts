02ad952f8b395749d527f9978718174360cb0393
Rémi Connesson <remiconnesson2@gmail.com>
2025-11-25 03:10:45 +0000

    Delete unused code

--- db/index.ts
+++
@@ -1,9 +0,0 @@
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
-// import * as schema from "@/db/schema";
-import "dotenv/config";
-
-const sql = neon(process.env.DATABASE_URL!);
-// const db = drizzle(sql, { schema });
-
-export default db;
